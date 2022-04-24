ebbc2da4dbb6064d8b083d927c562b6944842680
cat-master21 <96554164+cat-master21@users.noreply.github.com>
2022-04-24 15:42:51 -0700

    fix(script.sh): use sudo more

--- script.sh
+++ script.sh
@@ -4,20 +4,20 @@ apt update -y
 apt upgrade -y
 apt install git
 apt autoremove -y
-mv ~/creation_script/assets/rolling-rhino.png /usr/share/backgrounds
-mv ~/creation_script/assets/.bashrc /etc/skel
-mv ~/creation_script/assets/.bash_aliases /etc/skel
-mv ~/creation_script/assets/.sources.sh /etc/skel
-rm -rf /etc/os-release
-mv ~/creation_script/assets/os-release /etc
-rm -rf /usr/share/glib-2.0/schemas/10_ubuntu-settings.gschema.override 
-mv ~/creation_script/assets/10_ubuntu-settings.gschema.override /usr/share/glib-2.0/schemas
+sudo mv ~/creation_script/assets/rolling-rhino.png /usr/share/backgrounds
+sudo mv ~/creation_script/assets/.bashrc /etc/skel
+sudo mv ~/creation_script/assets/.bash_aliases /etc/skel
+sudo mv ~/creation_script/assets/.sources.sh /etc/skel
+sudo rm -rf /etc/os-release
+sudo mv ~/creation_script/assets/os-release /etc
+sudo rm -rf /usr/share/glib-2.0/schemas/10_ubuntu-settings.gschema.override 
+sudo mv ~/creation_script/assets/10_ubuntu-settings.gschema.override /usr/share/glib-2.0/schemas
 # Finish with rhino-config 
 mkdir ~/creation_script/rhino-config
 cd ~/creation_script/rhino-config
 wget # Link goes here
 chmod +x ~/creation_script/rhino-config/rhino-config
-mv ~/creation_script/rhino-config/rhino-config /usr/bin
+sudo mv ~/creation_script/rhino-config/rhino-config /usr/bin
 # Alert user that the script has completed
 echo "SCRIPT HAS COMPLETED - DELETING SCRIPT FILES"
 echo "DO NOT DELETE GIT FROM SYSTEM"
