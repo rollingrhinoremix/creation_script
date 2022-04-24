6d38745b80d815b48e392a4d1a167bf15028eed8
cat-master21 <96554164+cat-master21@users.noreply.github.com>
2022-04-24 15:21:56 -0700

    fix(script.sh): use sudo with mv

--- script.sh
+++ script.sh
@@ -4,10 +4,10 @@ apt update -y
 apt upgrade -y
 apt install git
 apt autoremove -y
-mv ~/creation_script/assets/rolling-rhino.png /usr/share/backgrounds
-mv ~/creation_script/assets/.bashrc /etc/skel
-mv ~/creation_script/assets/.bash_aliases /etc/skel
-mv ~/creation_script/assets/.sources.sh /etc/skel
+sudo mv ~/creation_script/assets/rolling-rhino.png /usr/share/backgrounds
+sudo mv ~/creation_script/assets/.bashrc /etc/skel
+sudo mv ~/creation_script/assets/.bash_aliases /etc/skel
+sudo mv ~/creation_script/assets/.sources.sh /etc/skel
 rm -rf /etc/os-release
 mv ~/creation_script/assets/os-release /etc
 rm -rf /usr/share/glib-2.0/schemas/10_ubuntu-settings.gschema.override 
@@ -16,7 +16,7 @@ mv ~/creation_script/assets/10_ubuntu-settings.gschema.override /usr/share/glib-
 mkdir ~/creation_script/rhino-config
 git clone https://github.com/rollingrhinoremix/rhino-config ~/creation_script/rhino-config
 chmod +x ~/creation_script/rhino-config/config.py
-mv ~/creation_script/rhino-config/config.py /usr/bin/rhino-config
+sudo mv ~/creation_script/rhino-config/config.py /usr/bin/rhino-config
 # Alert user that the script has completed
 echo "SCRIPT HAS COMPLETED - DELETING SCRIPT FILES"
 echo "DO NOT DELETE GIT FROM SYSTEM"
