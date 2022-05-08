a75d336ee026d98a77732fb77e4568ec1a806fe2
Alejandro Cervera <96702705+tricantivu@users.noreply.github.com>
2022-05-08 09:33:32 -0500

    style: use brace expansion to move files to /etc/skel

--- script.sh
+++ script.sh
@@ -11,19 +11,21 @@ which git &> /dev/null || {
 git clone https://github.com/rollingrhinoremix/distro ~/creation_script/assets
 
 apt autoremove -y
+
 mv ~/creation_script/assets/rolling-rhino.png /usr/share/backgrounds
-mv ~/creation_script/assets/.bashrc /etc/skel
-mv ~/creation_script/assets/.bash_aliases /etc/skel
-mv ~/creation_script/assets/.sources.sh /etc/skel
+mv ~/creation_script/assets/.{bashrc,bash_aliases,sources.sh} /etc/skel
+
 rm -rf /etc/os-release
 mv ~/creation_script/assets/os-release /etc
 rm -rf /usr/share/glib-2.0/schemas/10_ubuntu-settings.gschema.override 
 mv ~/creation_script/assets/10_ubuntu-settings.gschema.override /usr/share/glib-2.0/schemas
+
 # Finish with rhino-config 
 cd ~/creation_script/rhino-config
 wget https://github.com/rollingrhinoremix/rhino-config/releases/download/v2.0/rhino-config
 chmod +x ~/creation_script/rhino-config/rhino-config
 mv ~/creation_script/rhino-config/rhino-config /usr/bin
+
 # Alert user that the script has completed
 echo "SCRIPT HAS COMPLETED - DELETING SCRIPT FILES"
 echo "DO NOT DELETE GIT FROM SYSTEM"
